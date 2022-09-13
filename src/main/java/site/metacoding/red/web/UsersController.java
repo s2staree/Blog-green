c871e0d34d22986a5faf5a8a394cd3fc2a6a8fbc
s2staree <s2staree@gmail.com>
2022-09-13 19:38:05 +0900

    19:35 - 과제 시작

--- src/main/java/site/metacoding/red/web/UsersController.java
+++ src/main/java/site/metacoding/red/web/UsersController.java
@@ -2,6 +2,7 @@ package site.metacoding.red.web;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.red.domain.users.Users;
@@ -20,7 +20,7 @@ import site.metacoding.red.web.dto.request.users.LoginDto;
 import site.metacoding.red.web.dto.request.users.UpdateDto;
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class UsersController {
 
 	private final UsersService usersService;
