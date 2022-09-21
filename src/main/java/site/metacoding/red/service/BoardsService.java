85e4cda7f5a4724a32dea58cf6cef037b29c2545
s2staree <s2staree@gmail.com>
2022-09-21 12:11:24 +0900

    22-09-21 11:50 자바 예외처리 한방에 하기 (커스텀 익셉션)

--- src/main/java/site/metacoding/red/service/BoardsService.java
+++ src/main/java/site/metacoding/red/service/BoardsService.java
@@ -3,7 +3,6 @@ package site.metacoding.red.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.red.domain.boards.Boards;
@@ -11,6 +10,7 @@ import site.metacoding.red.domain.boards.BoardsDao;
 import site.metacoding.red.domain.loves.Loves;
 import site.metacoding.red.domain.loves.LovesDao;
 import site.metacoding.red.domain.users.Users;
+import site.metacoding.red.handler.ex.MyException;
 import site.metacoding.red.web.dto.request.boards.UpdateDto;
 import site.metacoding.red.web.dto.request.boards.WriteDto;
 import site.metacoding.red.web.dto.response.boards.DetailDto;
@@ -58,7 +58,14 @@ public class BoardsService {
 	}
 	
 	public Boards 게시글수정화면데이터가져오기(Integer id) {
-		return boardsDao.findById(id);
+		
+		Boards boards = boardsDao.findById(id);
+		
+		if(boards == null) {
+			throw new MyException(id + "의 게시글을 찾을 수 없습니다.");
+		}
+		
+		return boards;
 	}
 
 	public void 게시글수정하기(Integer id, UpdateDto updateDto) {
@@ -67,6 +74,7 @@ public class BoardsService {
 		
 		if(boardsPS == null) {
 			// 이 부분은 나중에 처리!! (exception 처리하는 법 따로 배울 예정)
+			throw new RuntimeException();	// catch로부터 복기함
 		}
 
 		// 2. 변경
